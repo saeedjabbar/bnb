c9f4def3680fce31920dae200bf500b202f79929
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-20 14:31:35 -0500

    rebase

--- src/App.js
+++ src/App.js
@@ -2,28 +2,24 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import {data} from  "./data"
 
+console.log(data);
 
 export default function App() {
   // <Hero />
-  return (
-    <div>
+  const experiences = data.map(experience => 
+    <Card
+      key={experience.id}
+      experience={experience}
+    />)
+    return (
+    <>
       <Navbar />
       <Hero />
-      <Card 
-        img="https://a0.muscache.com/im/pictures/lombard/MtTemplate-2660264-media_library/original/afdb188c-80b4-48ab-bb65-2b0a99a7dbc3.jpeg?im_w=1440"
-        rating="5.0"
-        reviewCount={116}
-        title="Circus Show"
-        price={137}
-      />
-      <Card
-        img="https://a0.muscache.com/im/pictures/e9c993c4-9de7-46b9-b8ea-f8872f474997.jpg?im_w=720"
-        rating="4.0"
-        reviewCount={16}
-        title="Ski Trip"
-        price={19}
-      />
-    </div>
+      <section className="cards-list">
+          {experiences}
+      </section>
+    </>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,27 @@
 import React from "react"
 
 export default function Card(props) {
+  let badgeText
+  if (props.experience.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  } else if (props.experience.location === "Online") {
+    badgeText = "ONLINE"
+  }
+
   return (
     <div className="card">
-      <img src={props.img} width="250" className="card--image" alt="" />
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+
+      <img src={props.experience.coverImg} width="250" className="card--image" alt="" />
+
       <div className="card--stats">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Red_star.svg/220px-Red_star.svg.png" width="15"  className="card--star" alt="" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">USA</span>
+        <span>{props.experience.stats.rating}</span>
+        <span className="gray">({props.experience.stats.reviewCount}) • </span>
+        <span className="gray">{props.experience.location}</span>
       </div>
-      <p>Life Lessons with Katie Zaferes</p>
-      <p><span className="bold">From ${props.price}</span> / person</p>
+      <p>{props.experience.title}</p>
+      <p><span className="bold">From ${props.experience.price}</span> / person</p>
     </div>
   )
 }
